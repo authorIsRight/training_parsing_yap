1b72342cdc30e2297e178113b062a4c4ca9b1022
Илья Соболев <i4sobolev@yandex.ru>
2023-03-04 17:55:33 +0100

    first ex solved

---
+++ caching.py
@@ -0,0 +1,19 @@
+import requests_cache
+
+# Сохранение адреса веб-страницы в константу. Это удобно, потому 
+# что при написании парсера, к адресу нужно постоянно обращаться. 
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+
+if __name__ == '__main__':
+    session = requests_cache.CachedSession()
+    response = session.get(MAIN_DOC_URL)
+
+    # Очистка кеша.
+#    session.cache.clear()
+
+
+    # Загрузка веб-страницы при помощи HTTP-метода get().
+
+    
+    print(session.cache.urls())
